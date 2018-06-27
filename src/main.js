445efeec900656402086646cdbdd1d64d43ca1a5
Isravel <jeshurunisravel@gmail.com>
2018-06-27 10:37:20 +0530

    initial commit

---
+++ src/main.js
@@ -0,0 +1,94 @@
+import JQuery from './js/jquery.js'
+// import SQL from './js/sql.js'
+// Import Vue
+import Vue from 'vue';
+
+// Import F7
+import Framework7 from 'framework7/dist/framework7.esm.bundle.js';
+
+// Import F7 Vue Plugin
+import Framework7Vue from 'framework7-vue/dist/framework7-vue.esm.bundle.js';
+
+// Import F7 Styles
+import Framework7Styles from 'framework7/dist/css/framework7.css';
+
+// Import Icons and App Custom Styles
+import IconsStyles from './css/icons.css';
+import AppStyles from './css/app.css';
+
+// Import Routes
+
+import Routes from './routes.js'
+
+// Import App Component
+import App from './app';
+
+// Init F7 Vue Plugin
+Vue.use(Framework7Vue, Framework7)
+
+// Init App
+new Vue({
+  el: '#app',
+  template: '<app/>',
+  // Init Framework7 by passing parameters here
+  framework7: {
+    id: 'io.framework7.testapp', // App bundle ID
+    name: 'CII Hymns', // App name
+    theme: 'auto', // Automatic theme detection
+    // App routes
+    routes: Routes,
+  },
+  // Register App Component
+  components: {
+    app: App
+  }
+});
+var sql = require('sql.js');
+// or
+// var sql = window.SQL
+// if you are in a browser
+
+// Create a database
+var db = new sql.Database();
+// NOTE: You can also use new sql.Database(data) where
+// data is an Uint8Array representing an SQLite database file
+
+// Execute some sql
+var sqlstr = "CREATE TABLE hello (a int, b char);";
+sqlstr += "INSERT INTO hello VALUES (0, 'hello');"
+sqlstr += "INSERT INTO hello VALUES (1, 'world');"
+db.run(sqlstr); // Run the query without returning anything
+
+var res = db.exec("SELECT * FROM hello");
+/*
+[
+	{columns:['a','b'], values:[[0,'hello'],[1,'world']]}
+]
+*/
+
+// Prepare an sql statement
+var stmt = db.prepare("SELECT * FROM hello WHERE a=:aval AND b=:bval");
+
+// Bind values to the parameters and fetch the results of the query
+var result = stmt.getAsObject({':aval' : 1, ':bval' : 'world'});
+console.log(result); // Will print {a:1, b:'world'}
+
+// Bind other values
+stmt.bind([0, 'hello']);
+while (stmt.step()) console.log(stmt.get()); // Will print [0, 'hello']
+
+// You can also use javascript functions inside your SQL code
+// Create the js function you need
+function add(a, b) {return a+b;}
+// Specifies the SQL function's name, the number of it's arguments, and the js function to use
+db.create_function("add_js", add);
+// Run a query in which the function is used
+db.run("INSERT INTO hello VALUES (add_js(7, 3), add_js('Hello ', 'world'));"); // Inserts 10 and 'Hello world'
+
+// free the memory used by the statement
+stmt.free();
+// You can not use your statement anymore once it has been freed.
+// But not freeing your statements causes memory leaks. You don't want that.
+
+// Export the database to an Uint8Array containing the SQLite database file
+var binaryArray = db.export();
---
+++ src/pages/home.vue
@@ -0,0 +1,33 @@
+<template>
+<f7-page>
+  <f7-navbar>
+    <f7-nav-left>
+      <f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" panel-open="left"></f7-link>
+    </f7-nav-left>
+    <f7-nav-title>My App</f7-nav-title>
+    <f7-nav-right>
+      <f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" panel-open="right"></f7-link>
+    </f7-nav-right>
+  </f7-navbar>
+  <f7-toolbar>
+    <f7-link>Left Link</f7-link>
+    <f7-link>Right Link</f7-link>
+  </f7-toolbar>
+  <f7-block strong>
+    <p>Here is your blank Framework7 app. Let's see what we have here.</p>
+  </f7-block>
+  <f7-block-title>Navigation</f7-block-title>
+  <f7-list id="songlist">
+    <f7-list-item link="/about/" title="About"></f7-list-item>
+    <f7-list-item link="/form/" title="Form"></f7-list-item>
+    <f7-list-item link="/dynamic-route/blog/45/post/125/?foo=bar#about" title="Dynamic Route"></f7-list-item>
+    <f7-list-item link="/load-something-that-doesnt-exist/" title="Default Route (404)"></f7-list-item>
+
+  </f7-list>
+</f7-page>
+</template>
+<script>
+export default {
+  
+}
+</script>
